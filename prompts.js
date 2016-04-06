6c6469f16a211f0c6ca7ea0c8fefa2225689a03d
kanimuni <kani_munidasa@vielabs.com>
2016-04-06 11:28:27 -0700

    added array concat to the excercise

--- prompts.js
+++ prompts.js
@@ -1,31 +1,44 @@
 var str = "Hello";
 var str2 = str;
-//What is str2?
-//console.log(str2);
+//What is str2? "Hello"
+// console.log(str2);
 
 str.concat(" World");
-// What is str?
-//console.log(str);
-// What is str2
-//console.log(str2);
+// What is str? "Hello"
+// console.log(str);
+// What is str2 "Hello"
+// console.log(str2);
 
 str = str.concat(" World");
-//What is str2?
-//console.log(str2);
+//What is str2? "Hello"
+// console.log(str2);
+
+
+var arr1 = [1,2,3,4,5];
+var arr2 = arr1;
+//What is arr2?  [1,2,3,4,5]
+// console.log(arr2);
+
+arr1.concat([6]);
+// What is arr1? [1,2,3,4,5]
+console.log(arr1);
+// What is arr2 [1,2,3,4,5]
+console.log(arr2);
+
 
 var arr = [1,2,3,4,5];
 var anotherArray = arr;
 arr.push(6);
-//What is arr?
-//console.log(arr)
-//What is anotherArray?
-//console.log(anotherArray)
+//What is arr? [1,2,3,4,5,6]
+// console.log(arr);
+//What is anotherArray? [1,2,3,4,5,6]
+// console.log(anotherArray);
 
 arr = [1,2,3,4,5,6,7];
-// What is anotherArray?
-//console.log(anotherArray)
+// What is anotherArray? [1,2,3,4,5,6]
+// console.log(anotherArray);
 
-// Why is the function below considered impure?
+// Why is the function below considered impure? //because your mutating the input.
 var addOne = function(numOrArray) {
   if(Array.isArray(numOrArray)) {
     numOrArray.push(1);
@@ -41,7 +54,22 @@ var array = [1,2,3];
 addOne(num);
 addOne(array);
 
-//console.log(num);
-//console.log(array);
+// console.log(num); // 5
+// console.log(array);  //[1,2,3,1]
 
 //How do we make changes to an array without mutating it? Can you refactor addOne to make it a pure function?
+
+var addOneNoMutation = function(numOrArray) {
+  if(Array.isArray(numOrArray)) {
+    var result = numOrArray.slice();
+    result.push(1);
+    return result;
+  } else {
+    numOrArray++;
+    return numOrArray;
+  }
+};
+
+var array = [1,2,3];
+// console.log( addOneNoMutation(array) );
+// console.log(array);
